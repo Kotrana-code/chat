f38c22e78aff32c701f8124db3364fa73a2d55cd
Ombre2 <64099442+Ombre2@users.noreply.github.com>
2022-03-02 09:29:36 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,73 @@
+
+const { Server } = require("socket.io");
+
+var app = require("./app");
+
+var moment = require("moment");
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+const IP = process.env.IP || "localhost";
+
+// then start listening
+const server = app.listen(PORT, function (error) {
+    if (error) {
+        console.error("Unable to listen for connections", error);
+        process.exit(10);
+    }
+    console.info(
+        "Server is listening on http://" +
+        IP +
+        ":" +
+        PORT
+    );
+});
+
+
+// Ici le code pour le socket 
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    },
+    allowEIO3: true, // tweaking it may help
+});
+
+var clients = [];
+
+//Whenever someone connects this gets executed
+io.on('connection', function (socket) {
+
+    socket.on('storeClientInfo', function (data) {
+        var clientInfo = new Object();
+        clientInfo.customId = data.customId;
+        clientInfo.clientId = socket.id;
+        clients.push(clientInfo);
+    });
+
+    console.log("Un utilisateur connected");
+    //Whenever someone disconnects this piece of code executed
+    socket.on('disconnect', function (reason) {
+        for (var i = 0, len = clients.length; i < len; ++i) {
+            var c = clients[i];
+            if (c.clientId == socket.id) {
+                clients.splice(i, 1);
+                break;
+            }
+        }
+    });
+
+    socket.on('sendMessage', function (data) {
+        for (var i = 0, len = clients.length; i < len; ++i) {
+            var c = clients[i];
+            if (c.customId == data.customId) {
+                // io.to(c.clientId).emit("receiveMesssage", data);
+                io.emit("receiveMesssage", data);
+                break;
+            }
+        }
+    })
+})
